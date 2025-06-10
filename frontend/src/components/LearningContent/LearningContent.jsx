892915571da52438a7abab4b0c0416e498ec2ce5
DisturbedCherry <jakub.t.wislicki@gmail.com>
2025-06-10 22:00:27 +0200

    added images

--- frontend/src/components/LearningContent/LearningContent.jsx
+++ frontend/src/components/LearningContent/LearningContent.jsx
@@ -1,6 +1,12 @@
 import styles from './LearningContent.module.css'
 import Card from '../Card/Card';
-import Sign from '../../assets/sign.jpg'
+
+import a from '../../assets/a.jpg'
+import b from '../../assets/b.jpg'
+import c from '../../assets/c.jpg'
+import e from '../../assets/e.jpg'
+import i from '../../assets/i.jpg'
+import l from '../../assets/l.jpg'
 
 function LearningContent() {
     return (
@@ -8,14 +14,14 @@ function LearningContent() {
             <div className={styles.section}>
                 Głoski Statyczne
                 <div className={styles.row}>
-                    <Card name='Lekcja A' image={Sign} borderColor='#00ADB5'/>
-                    <Card name='Lekcja B' image={Sign} borderColor='#00ADB5'/>
-                    <Card name='Lekcja C' image={Sign} borderColor='#00ADB5'/>
+                    <Card name='Lekcja A' image={a} borderColor='#EAC435'/>
+                    <Card name='Lekcja B' image={b} borderColor='#00ADB5'/>
+                    <Card name='Lekcja C' image={c} borderColor='#00ADB5'/>
                 </div>
                 <div className={styles.row}>
-                    <Card name='Lekcja D' image={Sign} borderColor='#00ADB5'/>
-                    <Card name='Lekcja E' image={Sign} borderColor='#EAC435'/>
-                    <Card name='Lekcja F' image={Sign} borderColor='#EAC435'/>
+                    <Card name='Lekcja E' image={e} borderColor='#00ADB5'/>
+                    <Card name='Lekcja I' image={i} borderColor='#00ADB5'/>
+                    <Card name='Lekcja L' image={l} borderColor='#00ADB5'/>
                 </div>
             </div>
         </div>
